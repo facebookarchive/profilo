684a607d4bce71248034652e595a54d471f000b1
Delyan Kratunov <delyank@fb.com>
2017-12-12 13:01:08 -0800

    Inline provider names
    Summary: Provider names are a config implementation detail.
    
    Differential Revision: D6546693
    
    fbshipit-source-id: 1803b69d7a1004a306756c645ab6f16a37b073aa

--- java/main/com/facebook/loom/core/LoomConstants.java
+++ java/main/com/facebook/loom/core/LoomConstants.java
@@ -2,11 +2,8 @@
 
 package com.facebook.loom.core;
 
-import com.facebook.fbtrace.utils.FbTraceId;
-
 final public class LoomConstants {
 
-  // User configurable providers
   public static final int PROVIDER_ASYNC =           1;
   public static final int PROVIDER_LIFECYCLE =       1 << 1;
   public static final int PROVIDER_QPL =             1 << 2;
@@ -27,12 +24,6 @@ final public class LoomConstants {
   public static final int PROVIDER_TRANSIENT_NETWORK_DATA = 1 << 17;
   public static final int PROVIDER_WALL_TIME_STACK_TRACE = 1 << 18;
   public static final int PROVIDER_LIBC_IO = 1 << 19;
-  // Should your new provider be added to ManualTraceController#evaluateConfig ?
-
-  // WARNING: Update PROVIDER_MAX whenever you add a provider!
-  public static final int PROVIDER_MAX = PROVIDER_LIBC_IO;
-  // Bit mask of all configurable providers
-  public static final int ALL_CONFIGURABLE_PROVIDERS = (PROVIDER_MAX << 1) - 1;  // 0x3F
 
   public static final int PROVIDER_LOOM_SYSTEM = 0xFFFFFFFF;
 
@@ -43,31 +34,6 @@ final public class LoomConstants {
   public static final int TRIGGER_MULTI_PROCESS =   1 << 4;
   public static final int TRIGGER_EXTERNAL = 1 << 5;
   public static final int TRIGGER_BLACK_BOX = 1 << 6;
-
-  public static final String PROVIDER_ASYNC_NAME = "async";
-  public static final String PROVIDER_LIFECYCLE_NAME = "lifecycle";
-  public static final String PROVIDER_QPL_NAME = "qpl";
-  public static final String PROVIDER_OTHER_NAME = "other";
-  public static final String PROVIDER_USER_COUNTERS_NAME = "user_counters";
-  public static final String PROVIDER_SYSTEM_COUNTERS_NAME = "system_counters";
-  public static final String PROVIDER_STACK_FRAME_NAME = "stack_trace";
-  public static final String PROVIDER_LIGER_NAME = "liger";
-  public static final String PROVIDER_MAJOR_FAULTS_NAME = "major_faults";
-  public static final String PROVIDER_THREAD_SCHEDULE_NAME = "thread_schedule";
-  public static final String PROVIDER_CLASS_LOAD_NAME = "class_load";
-  public static final String PROVIDER_NATIVE_STACK_TRACE_NAME = "native_stack_trace";
-  public static final String PROVIDER_JAVASCRIPT_STACK_TRACE_NAME = "javascript_stack_trace";
-  public static final String PROVIDER_MAIN_THREAD_MESSAGES_NAME = "main_thread";
-  public static final String PROVIDER_LIGER_HTTP2_NAME = "liger_http2";
-  public static final String PROVIDER_FBSYSTRACE_NAME = "fbsystrace";
-  public static final String PROVIDER_HIGH_FREQ_MAIN_THREAD_COUNTERS_NAME =
-      "high_freq_main_thread_counters";
-  public static final String PROVIDER_TRANSIENT_NETWORK_DATA_NAME = "transient_network_data";
-  public static final String PROVIDER_WALL_TIME_STACK_TRACE_NAME = "wall_time_stack_trace";
-  public static final String PROVIDER_LIBC_IO_NAME = "libc_io";
-
-  public static final int NO_TRACE = 0;
-  public static final String ENCODED_NO_TRACE = FbTraceId.NULL_ID;
 
   public static final int NO_CALLSITE = 0;
   public static final int NO_MATCH = 0;
