bccfe6de57498a86641fc6e25e8955d4b53285aa
Marc Horowitz <mhorowitz@fb.com>
2018-02-05 11:44:34 -0800

    move a lot of files around
    Summary:
    This diff moves everything fbjni needs into libraries/fbjni,
    and also moves the fbjni instrumentation tests into a separate test.
    It does not build or compile; the next diffs in the stack do that in
    pieces.
    
    Reviewed By: BurntBrunch
    
    Differential Revision: D5609683
    
    fbshipit-source-id: a4942fdd9e16539929c0cc15098d0a4976d293e1

--- deps/jni/HybridClassBase.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-package com.facebook.jni;
-
-import com.facebook.jni.annotations.DoNotStrip;
-
-@DoNotStrip
-public abstract class HybridClassBase extends HybridData {}
