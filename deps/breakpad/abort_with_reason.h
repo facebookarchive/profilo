9db6278cabc27d3c2b80e12141ecbfb41a983b12
Ricardo Rey <ricardorey@fb.com>
2018-04-30 16:04:40 -0700

    Opensource abortWithReason
    Summary: Opensource the abortWithReason() function.
    
    Reviewed By: BurntBrunch
    
    Differential Revision: D7765144
    
    fbshipit-source-id: ca172665b5d0282da336f4cf9c66903e769bb011

---
+++ deps/breakpad/abort_with_reason.h
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2004-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define __ABORT_MESSAGE_PREFIX(file, line)  file ":" line " "
+#define __TO_STR_INDIRECTED(x)  #x
+#define __TO_STR(x) __TO_STR_INDIRECTED(x)
+
+#define abortWithReason(msg)  abortWithReasonImpl(__ABORT_MESSAGE_PREFIX(__FILE__, __TO_STR(__LINE__)) msg)
+
+__attribute__((noreturn))
+void abortWithReasonImpl(const char* reason);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
