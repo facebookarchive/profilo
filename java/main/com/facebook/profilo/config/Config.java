d0ec93f8a048fc91d68853182f7162093f9d77b7
Delyan Kratunov <delyank@fb.com>
2020-08-26 12:52:27 -0700

    Remove RootControllersConfig
    Summary: deleteallthethings
    
    Reviewed By: aandreyeu
    
    Differential Revision: D23162223
    
    fbshipit-source-id: a0f89a282581b93b165e2b52e23944b756bb8d96

--- java/main/com/facebook/profilo/config/Config.java
+++ java/main/com/facebook/profilo/config/Config.java
@@ -19,23 +19,4 @@ public interface Config {
 
   @Nullable
   ConfigV2 getConfigV2();
-
-  interface RootControllerConfig {
-
-    @Nullable
-    ControllerConfig getConfigForController(int controller);
-
-    /**
-     * @return a maximum duration (timeout) for traces or -1 if none is set. The amount is in
-     *     milliseconds.
-     */
-    int getTraceTimeoutMs();
-
-    /**
-     * @return Sample rate at which to upload timed out traces. Value N means 1/N probability of
-     *     timed out trace to be uploaded. Default value is 0, which means that such traces should
-     *     be aborted.
-     */
-    int getTimedOutUploadSampleRate();
-  }
 }
