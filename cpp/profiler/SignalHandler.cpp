548595c5311e5d973a0be427f6b2d6f5a97e8fec
Delyan Kratunov <delyank@fb.com>
2020-09-08 12:40:37 -0700

    Introduce SignalHandler as a lightweight alternative to sigmux
    Summary:
    There are a couple of problems with our usage of sigmux.
    
    1. If we do in fact are the first library to initialize `sigmux`, we'd end up installing a `sigaction` wrapper which puts `sigmux` entirely in front of `sigchain` (and art's fault manager). This is in line with our goals and what we use `sigmux` for.
    a. Caveat: on Android 9+ this doesn't actually happen because of the distinction between `sigaction` and `sigaction64`. Ignore that for now.
    b. However, if we put `sigmux` in front of `sigchain` and someone *else* (e.g. fb-breakpad) registers with `sigmux`, we may break *their* expectations. For example, breakpad would run *before* art's fault handler, thus generating minidumps for recoverable faults (e.g. NPEs).
    2. If we are *not* the first library to initialize sigmux, then we are likely running *after* art's fault handling (as we can't assume that anyone has put sigmux in front of sigchain). This should be fine but isn't - art's fault handler can get confused and dereference garbage, causing a double fault and a consecutive crash.
    
    In this commit we introduce a profilo-specific solution for signal handling for the sampling profiler. It completely ignores sigmux and operates at the basic `sigaction` layer (by using libc's `sigaction` or `sigaction64` directly).
    
    We implement a much simpler and restricted version of sigmux:
    1) We can look up a specific `SignalHandler` from an execution of its actual signal handler function.
    2) We can wait for all active signals to finish processing (by using sigmux's own `phaser_t`)
    3) We can call the previous signal handler for this signal.
    
    We cannot remove a `SignalHandler` once installed as `SignalHandler` does not itself gain control of the signal when executing, it always passes the `HandlerPtr` to the `sigaction` call. The installed handlers are responsible for communicating with `SignalHandler` via the `EnterHandler` call and the corresponding methods on `HandlerScope`.
    
    Reviewed By: aandreyeu
    
    Differential Revision: D23248677
    
    fbshipit-source-id: 9fa0fd483576ef3710fd1b21b4c464c093968788

---
+++ cpp/profiler/SignalHandler.cpp
@@ -0,0 +1,232 @@
+/**
+ * Copyright 2004-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "SignalHandler.h"
+
+#include <dlfcn.h>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+
+#include <abort_with_reason.h>
+
+namespace facebook {
+namespace profilo {
+namespace profiler {
+
+std::atomic<SignalHandler*>
+    SignalHandler::globalRegisteredSignalHandlers[NSIG]{};
+
+SignalHandler& SignalHandler::Initialize(int signum, HandlerPtr handler) {
+  while (true) {
+    auto* lookup = globalRegisteredSignalHandlers[signum].load();
+    if (lookup) {
+      if (lookup->handler_ != handler) {
+        throw std::logic_error(
+            "SignalHandler::Initialize called with more than one handler!");
+      }
+      return *lookup;
+    }
+
+    // Lookup is null, let's try and instantiate the one and only SignalHandler
+    auto* instance = new SignalHandler(signum, handler);
+    if (globalRegisteredSignalHandlers[signum].compare_exchange_strong(
+            lookup, instance)) {
+      return *instance;
+    } else {
+      // Someone beat us to it, try again by re-reading lookup and
+      // re-verifying its state.
+      delete instance;
+      continue;
+    }
+  }
+}
+
+SignalHandler::SignalHandler(int signum, HandlerPtr handler)
+    : signum_(signum),
+      handler_(handler),
+      data_(nullptr),
+      phaser_(),
+      initialized_(false),
+      enabled_(false),
+      old_sigaction_() {
+  if (phaser_init(&phaser_)) {
+    throw std::runtime_error("Could not initialize phaser");
+  }
+}
+
+SignalHandler::~SignalHandler() {
+  phaser_destroy(&phaser_);
+}
+
+void SignalHandler::Enable() {
+  if (!initialized_) {
+    // Must take over the signal first.
+    AndroidAwareSigaction(signum_, handler_, &old_sigaction_);
+    initialized_ = true;
+  }
+  enabled_.store(true, std::memory_order_relaxed);
+}
+
+void SignalHandler::Disable() {
+  enabled_.store(false, std::memory_order_relaxed);
+  phaser_drain(&phaser_);
+}
+
+SignalHandler::HandlerScope SignalHandler::EnterHandler(int signum) {
+  auto* handler = globalRegisteredSignalHandlers[signum].load();
+  if (!handler) {
+    abortWithReason("EnterHandler call but no registered SignalHandler");
+  }
+
+  return SignalHandler::HandlerScope(*handler);
+}
+
+void SignalHandler::CallPreviousHandler(
+    int signum,
+    siginfo_t* info,
+    void* ucontext) {
+  if (!initialized_) {
+    return;
+  }
+
+  sigset_t oldsigs;
+  // Actually calls sigchain's sigprocmask wrapper but that's okay.
+  // The sigchain wrapper only really cares about not masking signals
+  // that sigchain has special handlers for. That type of mask modification
+  // is perfectly okay with us.
+  if (sigprocmask(SIG_SETMASK, &old_sigaction_.sa_mask, &oldsigs)) {
+    abortWithReason("Cannot change signal mask");
+  }
+  if (old_sigaction_.sa_flags & SA_SIGINFO) {
+    if (old_sigaction_.sa_sigaction != nullptr) {
+      old_sigaction_.sa_sigaction(signum, info, ucontext);
+    }
+  } else if (old_sigaction_.sa_handler != nullptr) {
+    if (old_sigaction_.sa_handler != SIG_DFL &&
+        old_sigaction_.sa_handler != SIG_IGN) {
+      old_sigaction_.sa_handler(signum);
+    }
+  }
+  if (sigprocmask(SIG_SETMASK, &oldsigs, nullptr)) {
+    abortWithReason("Cannot restore signal mask");
+  }
+}
+
+void SignalHandler::AndroidAwareSigaction(
+    int signum,
+    HandlerPtr handler,
+    struct sigaction* oldact) {
+#ifdef __ANDROID__
+  //
+  // On Android, we need to look up sigaction64 or sigaction directly from libc
+  // or otherwise we'll get the wrappers from sigchain.
+  //
+  // These wrappers do not work for our purposes because they run art's signal
+  // handling before our handler and that signal handling can be misled to
+  // believe it's in art code when in fact it's in SamplingProfiler unwinding.
+  //
+  // Further, we must use sigaction64 if available as otherwise we may hit a bug
+  // in bionic where sigaction`libc calls sigaction64`sigchain.
+  // See commit 11623dd60dd0f531fbc1cbf108680ba850acaf2f in AOSP.
+  //
+  auto libc = dlopen("libc.so", RTLD_LOCAL);
+  if (!libc) {
+    std::string error("Missing libc.so: ");
+    throw std::runtime_error(error + dlerror());
+  }
+
+  int (*libc_sigaction64)(int, const struct sigaction64*, struct sigaction64*) =
+      nullptr;
+  int (*libc_sigemptyset64)(sigset64_t*) = nullptr;
+  int (*libc_sigismember64)(sigset64_t*, int) = nullptr;
+  int (*libc_sigaction)(int, const struct sigaction*, struct sigaction*) =
+      nullptr;
+
+  libc_sigaction64 =
+      reinterpret_cast<decltype(libc_sigaction64)>(dlsym(libc, "sigaction64"));
+  if (libc_sigaction64) {
+    libc_sigemptyset64 = reinterpret_cast<decltype(libc_sigemptyset64)>(
+        dlsym(libc, "sigemptyset64"));
+
+    libc_sigismember64 = reinterpret_cast<decltype(libc_sigismember64)>(
+        dlsym(libc, "sigismember64"));
+  } else {
+    libc_sigaction =
+        reinterpret_cast<decltype(libc_sigaction)>(dlsym(libc, "sigaction"));
+  }
+
+  dlclose(libc);
+
+  int result = 0;
+  if (libc_sigaction64) {
+    //
+    // sigaction64 is available.
+    // Convert from struct sigaction to struct sigaction64 and back
+    // and call it directly.
+    //
+    // Note that the conversion from sigset64_t to sigset_t is lossy,
+    // we lose real-time signals!
+    //
+    struct sigaction64 action64 {
+      .sa_sigaction = handler, .sa_flags = kSignalHandlerFlags,
+    };
+
+    libc_sigemptyset64(&action64.sa_mask);
+    struct sigaction64 oldaction64;
+    result = libc_sigaction64(signum, &action64, &oldaction64);
+    struct sigaction oldaction {
+      .sa_flags = oldaction64.sa_flags,
+    };
+
+    if (oldaction.sa_flags & SA_SIGINFO) {
+      oldaction.sa_sigaction = oldaction64.sa_sigaction;
+    } else {
+      oldaction.sa_handler = oldaction64.sa_handler;
+    }
+
+    sigemptyset(&oldaction.sa_mask);
+    for (int i = 0; i < NSIG; i++) {
+      if (libc_sigismember64(&oldaction64.sa_mask, i)) {
+        sigaddset(&oldaction.sa_mask, i);
+      }
+    }
+    *oldact = oldaction;
+  } else {
+    struct sigaction action {
+      .sa_sigaction = handler, .sa_flags = kSignalHandlerFlags,
+    };
+    sigemptyset(&action.sa_mask);
+    result = libc_sigaction(signum, &action, oldact);
+  }
+
+  if (result != 0) {
+    throw std::system_error(errno, std::system_category());
+  }
+#else // not __ANDROID__
+  struct sigaction action {};
+  action.sa_flags = kSignalHandlerFlags;
+  action.sa_sigaction = handler;
+  sigemptyset(&action.sa_mask);
+  if (sigaction(signum, &action, oldact)) {
+    throw std::system_error(errno, std::system_category());
+  }
+#endif
+}
+
+} // namespace profiler
+} // namespace profilo
+} // namespace facebook
---
+++ cpp/profiler/SignalHandler.h
@@ -0,0 +1,189 @@
+/**
+ * Copyright 2004-present, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <phaser.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <atomic>
+
+namespace facebook {
+namespace profilo {
+namespace profiler {
+
+//
+// SignalHandler provides signal handling niceties for use by SamplingProfiler.
+// In particular, it has the following main facilities:
+//
+// 1. Ability to look up associated pointers from within a signal handler
+// context (Set/GetData).
+// 2. Stronger guarantees when disabling the signal handler (all concurrent
+// are guaranteed to have exited when Disable() returns).
+// 3. RAII helpers for use inside signal handlers (HandlerScope).
+// 4. Correct installation of the signal handler before all other signal
+// handlers when running on Android.
+//
+// Signal handlers should use the EnterHandler static method to announce
+// themselves and gain partial access to the relevant SignalHandler instance
+// (via the methods on the HandlerScope).
+//
+// Once a SignalHandler has been installed for a particular signal, the handler
+// function (i.e. HandlerPtr value passed to Initialize) *cannot* be changed.
+// This is the primary reason this implementation is only useful for
+// SamplingProfiler's purposes.
+//
+class SignalHandler {
+ public:
+  using HandlerPtr = void (*)(int, siginfo_t*, void*);
+
+  // 
+  // Enable processing of signals by this SignalHandler.
+  // Installs the signal handler, if not yet installed.
+  // Calls to HandlerScope::IsEnabled will return true after this.
+  //
+  void Enable();
+
+  // 
+  // Disables processing of signals by this SignalHandler.
+  // Blocks until all signal handlers running at the time of this call
+  // exit their respective HandlerScopes.
+  //
+  void Disable();
+
+  void SetData(void* data) {
+    data_.store(data, std::memory_order_relaxed);
+  }
+
+  class HandlerScope {
+   private:
+    explicit HandlerScope(SignalHandler& handler) : handler_(handler) {
+      enabled_ = false;
+      if (!handler_.initialized_) {
+        return;
+      }
+      if (!handler_.enabled_.load(std::memory_order_relaxed)) {
+        return;
+      }
+      phase_ = phaser_enter(&handler_.phaser_);
+      enabled_ = true;
+    }
+
+    SignalHandler& handler_;
+    bool enabled_;
+    phaser_phase phase_;
+
+    friend class SignalHandler;
+
+   public:
+    HandlerScope(HandlerScope&& other)
+        : handler_(other.handler_),
+          enabled_(other.enabled_),
+          phase_(other.phase_) {
+      other.enabled_ = false;
+    }
+    HandlerScope& operator=(HandlerScope&& other) {
+      enabled_ = other.enabled_;
+      phase_ = other.phase_;
+      other.enabled_ = false;
+      return *this;
+    }
+
+    HandlerScope(const HandlerScope&) = delete;
+    HandlerScope& operator=(const HandlerScope&) = delete;
+
+    virtual ~HandlerScope() {
+      if (enabled_) {
+        phaser_exit(&handler_.phaser_, phase_);
+      }
+    }
+
+    bool IsEnabled() const {
+      return enabled_;
+    }
+
+    void* GetData() const {
+      return handler_.data_.load(std::memory_order_relaxed);
+    }
+
+    //
+    // Exits this HandlerScope, then performs a siglongjmp call.
+    //
+    void siglongjmp(sigjmp_buf& env, int val) {
+      if (enabled_) {
+        phaser_exit(&handler_.phaser_, phase_);
+        enabled_ = false;
+      }
+      ::siglongjmp(env, val);
+    }
+
+    //
+    // Exits this HandlerScope, then calls the previous signal handler.
+    //
+    void CallPreviousHandler(int signum, siginfo_t* info, void* ucontext) {
+      if (enabled_) {
+        phaser_exit(&handler_.phaser_, phase_);
+        enabled_ = false;
+      }
+      handler_.CallPreviousHandler(signum, info, ucontext);
+    }
+  };
+
+  static SignalHandler& Initialize(int signum, HandlerPtr handler);
+
+  //
+  // Call first thing from a registered signal handler function.
+  //
+  static SignalHandler::HandlerScope EnterHandler(int signum);
+
+ private:
+  friend class SignalHandler::HandlerScope;
+  friend class SignalHandlerTestAccessor;
+
+  SignalHandler(int signum, HandlerPtr handler);
+  SignalHandler(const SignalHandler&) = delete;
+  SignalHandler& operator=(const SignalHandler&) = delete;
+  virtual ~SignalHandler();
+
+  static constexpr auto kSignalHandlerFlags =
+      SA_SIGINFO | SA_NODEFER | SA_ONSTACK | SA_RESTART;
+
+  // global storage to facilitate looking up a SignalHandler instance from
+  // within a signal handler context
+  static std::atomic<SignalHandler*> globalRegisteredSignalHandlers[NSIG];
+
+  int signum_;
+  HandlerPtr handler_;
+  std::atomic<void*> data_;
+
+  phaser_t phaser_;
+
+  bool initialized_;
+  std::atomic<bool> enabled_;
+
+  struct sigaction old_sigaction_;
+
+  static void AndroidAwareSigaction(
+      int signum,
+      HandlerPtr handler,
+      struct sigaction* oldact);
+
+  void CallPreviousHandler(int signum, siginfo_t*, void*);
+};
+
+} // namespace profiler
+} // namespace profilo
+} // namespace facebook
