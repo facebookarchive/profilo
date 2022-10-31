df8773a5a014cfd8f53cca0f6fa4ac7aa96d9517
Ben Batya <benbatya@meta.com>
2022-10-31 13:07:54 -0700

    Adding fmt to 3rd party
    Summary: Prebuilts for fmt
    
    Reviewed By: xwovr
    
    Differential Revision:
    D40761747
    
    LaMa Project: L1147969
    
    fbshipit-source-id: 291e163b5856dfa38e857183b79e2aa894a10777

---
+++ deps/fmt/run-msbuild.bat
@@ -0,0 +1,3 @@
+
+    
+    C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/MSBuild/Current/Bin/MSBuild.exe -p:FrameworkPathOverride="C:\Program Files\Reference Assemblies\Microsoft\Framework;.NETFramework\v4.0" %*
